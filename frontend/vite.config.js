f9e0bfa72a6131e7199c8e7e5ff0997039a27a35
Seelam-Mohith <seelammohith2222@gmail.com>
2026-07-12 11:46:43 +0530

    updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,11 +15,5 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 500,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
-      }
-    }
   }
 })
